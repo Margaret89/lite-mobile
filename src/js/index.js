e8d6706f319309f02be38fbec08412259d8e61dc
Margaret <match14@yandex.ru>
2021-06-27 21:25:36 +0300

    part header

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,44 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
-});
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// $(window).scroll(function(){
+// 	if($(this).scrollTop()>300){
+// 		$('.js-move-up').addClass('visible');
+// 	}else{
+// 		$('.js-move-up').removeClass('visible');
+// 	}
+// });
+// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+// hover main menu
+if($('.js-cat-menu-item').length){
+	$('.js-cat-menu-item').hover(
+		function() {
+			$('.js-cat-menu-item').removeClass('hover');
+			$(this).addClass('hover');
+		}
+	);
+
+	$('.js-cat-menu-close').hover(
+		function() {
+			$('.js-cat-menu-item').removeClass('hover');
+		}
+	);
+
+	$('.js-cat-menu').hover(
+		function() {},
+		function() {
+			$('.js-cat-menu-item').removeClass('hover');
+		}
+	);
+}
+
+// Открыть/Закрыть меню
+if($('.js-main-menu-cat').length){
+	$('.js-main-menu-cat').on('click', function() {
+		$('.js-cat-menu').toggleClass('active inactive');
+	});
+
+	$('.js-cat-menu-close').on('click', function() {
+		$('.js-cat-menu').toggleClass('active inactive');
+	});
+}
